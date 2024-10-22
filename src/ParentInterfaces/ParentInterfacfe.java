f9f4a8fa57e1e3c2844d00cca9009a7c4aa1a664
ujjwal dethe <ujwaldethe518@gmail.com>
2024-10-22 12:54:53 +0530

    Parent Interface Interaction.

---
+++ src/ParentInterfaces/ParentInterfacfe.java
@@ -0,0 +1,19 @@
+package ParentInterfaces;
+
+import java.util.*;
+
+public class ParentInterfacfe {
+    public static void main(String args[]) {
+        List list = new ArrayList();
+        Queue q = new ArrayDeque();
+
+        List li = new LinkedList();
+
+        Deque qr = new LinkedList();
+
+        Set set = new HashSet();
+
+        //Used Parent Class Refernce We Cannot access speciliazed methods of different subclasses.
+
+    }
+}
