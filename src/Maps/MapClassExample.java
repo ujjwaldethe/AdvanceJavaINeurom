8eb212f59da486cc1c98ba7830f65aa908d6f09b
ujjwal dethe <ujwaldethe518@gmail.com>
2024-10-23 21:02:21 +0530

    Maps Added.

--- src/Maps/MapClassExample.java
+++ src/Maps/MapClassExample.java
@@ -1,4 +1,14 @@
 package Maps;
 
+import java.util.HashMap;
+
 public class MapClassExample {
+    public static void main(String args){
+        MapClassExample n = new MapClassExample();
+        HashMap hm = new HashMap();
+        hm.put(1,"Ujjwal");
+        hm.put(2,"krishn");
+        hm.put(3,"JDAD");
+        System.out.print(hm);
+    }
 }
