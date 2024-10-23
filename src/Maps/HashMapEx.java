b325457aef5648b457453275112dc41f99d89156
ujjwal dethe <ujwaldethe518@gmail.com>
2024-10-23 21:06:19 +0530

    Maps Added.

---
+++ src/Maps/HashMapEx.java
@@ -0,0 +1,13 @@
+package Maps;
+import java.util.*;
+public class HashMapEx {
+public static void main(String agrs[])
+{
+    HashMap hm = new HashMap();
+    hm.put(1,"Ujjwal");
+    hm.put(2,"krishn");
+    hm.put(3,"JDAD");
+    System.out.print(hm);
+}
+
+}
