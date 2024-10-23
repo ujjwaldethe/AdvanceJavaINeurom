46bd1cf4b01f0384cb0754cb0a9011c929684bea
ujjwal dethe <ujwaldethe518@gmail.com>
2024-10-23 21:20:20 +0530

    edited

--- src/Maps/HashMapEx.java
+++ src/Maps/HashMapEx.java
@@ -7,13 +7,13 @@ public static void main(String agrs[])
     hm.put(1,"Ujjwal");
     hm.put(2,"krishn");
     hm.put(3,"JDAD");
-    System.out.print("HashMap : "+hm);
+    System.out.print("\nHashMap : "+hm);
 
     LinkedHashMap hm1 = new LinkedHashMap();
     hm1.put(1,"Ujjwal");
     hm1.put(2,"krishn");
     hm1.put(3,"JDAD");
-    System.out.print("LinkedHashMap : "+hm1);
+    System.out.print("\nLinkedHashMap : "+hm1);
 }
 
 }
