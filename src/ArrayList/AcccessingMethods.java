99954624d942a7fdeca2c8b6c0ecb02a75dc26fa
ujjwal dethe <ujwaldethe518@gmail.com>
2024-10-24 23:43:46 +0530

    Commits

--- src/ArrayList/AcccessingMethods.java
+++ src/ArrayList/AcccessingMethods.java
@@ -59,6 +59,7 @@ public class AcccessingMethods {
         {
             Integer i = (Integer) adit.next();//casting to integer object.
             System.out.print(i+" ");
+
         }
         LinkedList ll = new LinkedList();
         ll.add(10);
