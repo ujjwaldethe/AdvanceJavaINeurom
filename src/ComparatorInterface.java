854e5ebd63ebdb9a0d2ec0eb51ecd0562e953e05
ujjwal dethe <ujwaldethe518@gmail.com>
2024-10-30 12:04:01 +0530

    Comparator Interface

---
+++ src/ComparatorInterface.java
@@ -0,0 +1,13 @@
+import java.util.TreeSet;
+
+public class ComparatorInterface {
+    public static void main(String[] args) {
+        TreeSet s = new TreeSet();
+        s.add(10);
+        s.add(20);
+        s.add(0);
+        s.add(23);
+        s.add(1);
+        System.out.println(s);
+    }
+}
