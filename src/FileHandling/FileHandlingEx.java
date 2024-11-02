cadecc21f506da671498eba4a2fd1dad5e05618a
ujjwal dethe <ujwaldethe518@gmail.com>
2024-11-02 23:45:08 +0530

    File Handling

--- src/FileHandling/FileHandlingEx.java
+++ src/FileHandling/FileHandlingEx.java
@@ -16,21 +16,25 @@ class Student{
         return "id :"+sid+" name : "+sname+" age : "+age;
     }
 }
+//java.io api classes are built using the standards of unix os.
+//In linux/unix os there is no difference between file and directory
 public class FileHandlingEx {
     public static void main(String[] args) throws IOException {
 
         String fileName = "abc.txt";
+        String directortName = "IPL";
         /*
         * This line will check whether the file called "abc.txt" exists or not
         * if it is available then it would go and point to that file
         * otherwise it will represent a java file object, not a physical file.
         * */
-        File f = new File(fileName);
+        File f = new File("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src\\FileHandling",fileName);
+        File f1 = new File(directortName);
         System.out.println(f.exists());
         f.createNewFile();//it will create a physical file
-        //if the file does not eexists for th java file object
+        //if the file does not eeKxists for th java file object
         System.out.println(f.exists());
-
+        f1.mkdir();//This Will Create Directory
         Student s1 = new Student(1,"Ujjwal",22);
         Student s2 = new Student(2,"Krishna",23);
         System.out.println(s1+"\n"+s2);
