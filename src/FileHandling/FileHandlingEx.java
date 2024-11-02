bf430869ced26616e08373a57fcd58bafa46fdd7
ujjwal dethe <ujwaldethe518@gmail.com>
2024-11-03 00:32:57 +0530

    File Handling

--- src/FileHandling/FileHandlingEx.java
+++ src/FileHandling/FileHandlingEx.java
@@ -1,5 +1,6 @@
 package FileHandling;
 
+import javax.management.timer.Timer;
 import java.io.File;
 import java.io.IOException;
 
@@ -29,15 +30,44 @@ public class FileHandlingEx {
         * otherwise it will represent a java file object, not a physical file.
         * */
         File f = new File("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src\\FileHandling",fileName);
-        File f1 = new File(directortName);
+        File f1 = new File("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src\\FileHandling",directortName);
         System.out.println(f.exists());
         f.createNewFile();//it will create a physical file
-        //if the file does not eeKxists for th java file object
+        //if the file does not .exists for th java file object
         System.out.println(f.exists());
         f1.mkdir();//This Will Create Directory
+
+        System.out.println("Is File :  "+f.isFile());
+        System.out.println("Is Directory :  "+f.isDirectory());
+
         Student s1 = new Student(1,"Ujjwal",22);
         Student s2 = new Student(2,"Krishna",23);
         System.out.println(s1+"\n"+s2);
+
+        File f3 = new File("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src");
+        String[] names1 = f3.list();
+        int fcount=0,dcount=0;
+        for(String files : names1)
+        {
+            File fname = new File(f3,files);
+            if(fname.isFile())
+            {
+                fcount++;
+            }
+            if(fname.isDirectory())
+            {
+                dcount++;
+            }
+            if(files.endsWith(".txt"))
+            {
+                System.out.println("Exists");
+            }
+            System.out.println(files);
+        }
+        System.out.println("Total Files : "+fcount);
+        System.out.println("Total Directory : "+dcount);
+        f1.delete();//directory delete
+        //f.delete();//file deleted
 
     }
 }
---
+++ src/FileHandling/FileWriterEx.java
@@ -0,0 +1,11 @@
+package FileHandling;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterEx
+{
+    public static void main(String[] args) throws IOException {
+        FileWriter f = new FileWriter("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src\\FileHandling\\abc1.txt");
+    }
+}
