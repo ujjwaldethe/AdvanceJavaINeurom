43af1368c232f7228950b3f74156c41deec71f16
ujjwal dethe <ujwaldethe518@gmail.com>
2024-11-06 01:40:37 +0530

    Practise Using HashTable

--- src/FileHandling/Practise3.java
+++ src/FileHandling/Practise3.java
@@ -1,14 +1,23 @@
 package FileHandling;
 
 import java.io.*;
+import java.util.HashSet;
 
 public class Practise3 {
         public static void main (String[]args) throws IOException {
 
             BufferedReader b1 = new BufferedReader(new FileReader("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src\\FileHandling\\input.txt"));
-
             PrintWriter p = new PrintWriter(new FileWriter("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src\\FileHandling\\output.txt"));
-
+            HashSet<String> h = new HashSet<String>();
+            String line = b1.readLine();
+            while (line!=null){
+                h.add(line);
+                line=b1.readLine();
+            }
+            for(String s : h)
+            {
+                p.println(s);
+            }
             p.flush();
             b1.close();
             p.close();
