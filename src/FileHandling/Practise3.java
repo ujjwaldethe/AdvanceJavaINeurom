f70b1b68aeca3a5fac0c6bd6a4c4c95780e677a8
ujjwal dethe <ujwaldethe518@gmail.com>
2024-11-06 01:23:54 +0530

    Practise

--- src/FileHandling/Practise3.java
+++ src/FileHandling/Practise3.java
@@ -5,27 +5,13 @@ import java.io.*;
 public class Practise3 {
         public static void main (String[]args) throws IOException {
 
-            BufferedReader b1 = new BufferedReader(new FileReader("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src\\FileHandling\\file1.txt"));
+            BufferedReader b1 = new BufferedReader(new FileReader("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src\\FileHandling\\input.txt"));
 
-            PrintWriter p = new PrintWriter(new FileWriter("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src\\FileHandling\\file3.txt"));
+            PrintWriter p = new PrintWriter(new FileWriter("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src\\FileHandling\\output.txt"));
 
-            BufferedReader b2 = new BufferedReader(new FileReader("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src\\FileHandling\\file2.txt"));
-            String line = b1.readLine();
-            String line2 = b2.readLine();
-            while (line != null || line2 != null) {
-                if (line != null) {
-                    p.println(line);
-                    line = b1.readLine();
-                }
-                if (line2 != null) {
-                    p.println(line2);
-                    line2 = b2.readLine();
-                }
-            }
             p.flush();
             b1.close();
-            b2.close();
             p.close();
         }
-    }
+
 }
