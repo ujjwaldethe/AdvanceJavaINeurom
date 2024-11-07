7313e7748883616a3b2317935adcae3975c7c084
ujjwal dethe <ujwaldethe518@gmail.com>
2024-11-08 01:49:51 +0530

    Custom Serialization

--- src/SerializationAndDeSerialization/SerializationEx4.java
+++ src/SerializationAndDeSerialization/SerializationEx4.java
@@ -16,25 +16,17 @@ public class SerializationEx4 {
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(account);
 
-        System.out.println("Serialized Object Reference : " + account);
         System.out.println("Serialization Ended");
 
         System.out.println("De-serialization Started...");
         FileInputStream fis = new FileInputStream("C:\\Users\\krish\\IdeaProjects\\AdvanceJavaINeurom\\src\\SerializationAndDeSerialization\\abc3.ser");
         ObjectInputStream ois = new ObjectInputStream(fis);
         Object obj = ois.readObject();
-        /*
-        Object Creation Is Happening Internally Using Method Body : readObject()
-        Getting Old Object So Constructor Is Not Called.
-        State Of The Object Is Same i.e : Instance Variables*/
+
         Account acc = (Account) obj;
-        /*
-         * Error We Should Read Object In A Way That It Is Added
-         * Cat c1 =(Cat)obj2;
-         * Dog d1= (Dog) obj;
-         * */
+
         System.out.println("Username : "+acc.username+"\nPassword : "+acc.password);
-        System.out.println("De-Serialized Object Reference : " +acc);
+
         System.out.println("De-serialization Ended...");
     }
 }
