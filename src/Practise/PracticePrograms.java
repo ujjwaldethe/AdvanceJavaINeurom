42a7c6459d51b4d2a7bf1a6a3ee440806310c5a3
ujjwal dethe <ujwaldethe518@gmail.com>
2025-01-13 13:05:44 +0530

    Practising Fail Fast And Fail Safe.

---
+++ src/Practise/PracticePrograms.java
@@ -0,0 +1,20 @@
+package Practise;
+
+import java.util.*;
+
+public class PracticePrograms {
+    public static void main(String args[])
+    {
+        ArrayList al = new ArrayList();
+        al.add(100);
+        al.add(200);
+        al.add(300);
+        al.add(400);
+        al.add(500);
+        for(int i=0;i<al.size();i++)
+        {
+            System.out.println(al.get(i));
+            //al.add(300);
+        }
+    }
+}
