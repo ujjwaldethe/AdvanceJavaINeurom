35a52515d686859aa558dd9aa52368a079bbd626
ujjwal dethe <ujwaldethe518@gmail.com>
2025-01-15 22:16:45 +0530

    Enum Updated.

--- src/EnumAndAnnotations/EnumExample.java
+++ src/EnumAndAnnotations/EnumExample.java
@@ -1,11 +1,14 @@
 package EnumAndAnnotations;
+
+import java.security.DrbgParameters;
+
 //NORTH SOUTH EAST WEST
 //MALE FEMALE OTHERS
 //SUN MON TUE WED THU FRI SAT
 //JAN FEB MARCH ____ DEC
 //PASS FAIL NO RESULT
 enum Result{
-    PASS,FAIL,NR; //by default it is static and final
+    PASS,FAIL,NR; //bydefault it is static and final
     //fields ->instance variables ->properties.
     //methods
     //Constructor
@@ -18,8 +21,12 @@ enum Compass{
 }//FOR EACH ENUM SEPERATE .CLASS FILE IS GENERATED.
 public class EnumExample {
     public static void main(String[] args) {
-        enum Gender{
-            MALE,FEMALE,OTHERS;
+        Result res = Result.PASS;
+        System.out.println(res);
+        Result resArr[] = Result.FAIL.values();
+        for(Result dataex : resArr)
+        {
+            System.out.println(dataex.ordinal()+" : " +dataex);
         }
     }
 }
