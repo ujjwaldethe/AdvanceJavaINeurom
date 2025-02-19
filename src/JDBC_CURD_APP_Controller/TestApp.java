3876f657ec2562fe4f75077d53db0e4df37bdf81
ujjwal dethe <ujwaldethe518@gmail.com>
2025-02-19 13:24:52 +0530

    Layered Approach And Design Pattern Added.

--- src/JDBC_CURD_APP_Controller/TestApp.java
+++ src/JDBC_CURD_APP_Controller/TestApp.java
@@ -3,16 +3,34 @@ package JDBC_CURD_APP_Controller;
 import IStudentService.StudentServiceFactory;
 import JDBC_CURD_APP_Service.IStudentService;
 import JDBC_CURD_APP_dto.Student;
+import java.util.Scanner;
+
 //Controller Logic
 public class TestApp {
+
+
+
     public static void main(String[] args) {
+        String name;
+        Integer age;
+        String address;
+        String msg ;
         IStudentService studentService = StudentServiceFactory.getStudentService();
-        String msg = studentService.addStudent("Ujjwal",21,"MI");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name : ");
+        name = sc.next();
+        System.out.println("Enter Age : ");
+        age = sc.nextInt();
+        System.out.println("Enter Address : ");
+        address = sc.next();
+
+        msg = studentService.addStudent(name,age,address);
         if(msg.equalsIgnoreCase("success"))
         {
             System.out.println("Record Inserted Successfully");
         }
-        else {
+        else
+        {
             System.out.println("Record Insertion UnSuccessful");
         }
     }
