2f235e246730cb9d24566232ea7860fab3a941de
ujjwal dethe <ujwaldethe518@gmail.com>
2025-02-19 11:18:40 +0530

    Callable Statement

--- src/JDBC/JDBCCallableStatements.java
+++ src/JDBC/JDBCCallableStatements.java
@@ -7,13 +7,17 @@ import java.sql.Types;
 import java.util.Scanner;
 
 public class JDBCCallableStatements {
-    private  static  final String sPC = "{CALL P_GET_PRODUCT_DETAILS_BY_ID (?,?,?,?)}";
+    private  static  final String sPC1 = "{CALL P_GET_PRODUCT_DETAILS_BY_ID (?,?,?,?)}";
+    private  static  final String sPC2 = "{CALL P_GET_PRODUCT_BY_NAME (?,?)}";
     public static void main(String[] args) {
         Connection conn = null;
-        CallableStatement cs = null;
+        CallableStatement cs1 = null;
+        CallableStatement cs2 = null;
         ResultSet rs = null;
         String region = null;
         Integer id = null;
+        String name1 = null;
+        String name2 = null;
         //CALL P_GET_PRODUCT_DETAILS_BY_ID(1, @name, @rate, @qnt);
 
         try
@@ -21,32 +25,57 @@ public class JDBCCallableStatements {
             conn = JDBCStandardApp2.getJDBCConnection();
             if(conn!=null)
             {
-                cs = conn.prepareCall(sPC);
+                cs1 = conn.prepareCall(sPC1);
+                cs2 =conn.prepareCall(sPC2);
             }
             //Setting Input Values
-            if(cs!=null)
+            if(cs1!=null)
             {
                 System.out.println("Enter Id : ");
                 id = new Scanner(System.in).nextInt();
-                cs.setInt(1,id);
+                cs1.setInt(1,id);
+            }
+            if(cs2!=null)
+            {
+                System.out.println("Enter Name 1 : ");
+                name1 = new Scanner(System.in).next();
+                System.out.println("Enter Name 2 : ");
+                name2 = new Scanner(System.in).next();
+
+            }
+            if(cs2!=null) {
+                cs2.setString(1, name1);
+                cs2.setString(2, name2);
             }
             ///Setting Output Values
-            if(cs!=null)
+            if(cs1!=null)
             {
-                cs.registerOutParameter(2, Types.VARCHAR);
-                cs.registerOutParameter(3,Types.INTEGER);
-                cs.registerOutParameter(4,Types.INTEGER);
+                cs1.registerOutParameter(2, Types.VARCHAR);
+                cs1.registerOutParameter(3,Types.INTEGER);
+                cs1.registerOutParameter(4,Types.INTEGER);
             }
-            if(cs!=null)
+            if(cs1!=null)
             {
-                cs.execute();
+                cs1.execute();
+            }
+            if(cs2!=null)
+            {
+                cs2.execute();
+                rs = cs2.getResultSet();
             }
             //Retrieve The Result
-            if(cs!=null)
+            if(cs1!=null)
             {
-                System.out.println("Product Name : "+cs.getString(2));
-                System.out.println("Product Cost : "+cs.getInt(3));
-                System.out.println("Product Quantity : "+cs.getInt(4));
+                System.out.println("Product Name : "+cs1.getString(2));
+                System.out.println("Product Cost : "+cs1.getInt(3));
+                System.out.println("Product Quantity : "+cs1.getInt(4));
+            }
+            if(rs!=null)
+            {
+                while(rs.next())
+                {
+                    System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getInt(3)+"\t"+rs.getInt(4));
+                }
             }
         }
         catch (Exception e)
