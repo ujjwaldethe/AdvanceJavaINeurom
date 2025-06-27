b5389647a832c0182a3a0507413e6b31478c1d8c
ujjwal dethe <ujwaldethe518@gmail.com>
2025-06-27 22:08:47 +0530

    Interface (Runtime Polymorphism ,Loose Coupling, JDBC API )

---
+++ src/Interfaces/Ex5/TestApp.java
@@ -0,0 +1,20 @@
+package Interfaces.Ex5;
+
+interface IRemote
+{
+    //SRS
+
+
+    int MIN_VOLUME = 0; //Common For All Implementation class
+    int MAX_VOLUME = 15;//Common For All Implementation class
+}
+class Samsung implements IRemote
+{
+
+}
+public class TestApp {
+    int a;
+    public static void main(String[] args) {
+
+    }
+}
