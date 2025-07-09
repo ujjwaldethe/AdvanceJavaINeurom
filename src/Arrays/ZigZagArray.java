c4a3406dced4e13cb1a09f643ace701c64b70869
ujjwal dethe <ujwaldethe518@gmail.com>
2025-07-09 23:34:52 +0530

    Arrays

--- src/Arrays/ZigZagArray.java
+++ src/Arrays/ZigZagArray.java
@@ -1,25 +1,26 @@
 package Arrays;
+
 import java.util.Arrays;
 
-public class MergeAndFirstHalfReverse {
+public class ZigZagArray {
     public static void main(String[] args) {
         int [] ar1 = {1,2,3,4,5,6};
         int [] ar2 = {7,8,9,10,11};
-        System.out.println(Arrays.toString(mergeArray(ar1,ar2)));
+        System.out.println(Arrays.toString(zigZag(ar1,ar2)));
     }
-    public static int[] mergeArray(int[] ar1,int[] ar2)
+    public static int[] zigZag(int[] ar1,int[] ar2)
     {
+        int minLength = (ar1.length < ar2.length) ? ar1.length : ar2.length;
+
         int ans[] = new int[(ar1.length+ar2.length)];
         for(int i =0 ; i < ans.length;i++)
         {
-            if(i<ar1.length)
+            if(i<minLength)
             {
                 ans[i]=ar1[i];
+                ans[i+1]=ar2[i];
             }
-            else
-            {
-                ans[i]=ar2[ans.length-(i+1)];
-            }
+
         }
         return ans;
     }
