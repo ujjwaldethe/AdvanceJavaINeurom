15f5093403f44dfa7ddb8bc7991b06fc0b7643b3
ujjwal dethe <ujwaldethe518@gmail.com>
2025-07-10 13:18:22 +0530

    Zig Zag Array

--- src/Arrays/ZigZagArray.java
+++ src/Arrays/ZigZagArray.java
@@ -4,45 +4,31 @@ import java.util.Arrays;
 
 public class ZigZagArray {
     public static void main(String[] args) {
-        int [] ar1 = {1,2,3,4,5,6,6};
-        int [] ar2 = {7,8,9,10,11};
-        System.out.println("test");
+        int [] ar1 = {1,2,3,4,5,6,7,8};
+        int [] ar2 = {1,2,3,4,5,6,7,8,9};
         System.out.println(Arrays.toString(zigZag(ar1,ar2)));
-        System.out.println("test");
     }
-    public static int[] zigZag(int[] ar1,int[] ar2)
-    {
+    public static int[] zigZag(int[] ar1,int[] ar2) {
         int minLength = (ar1.length < ar2.length) ? ar1.length : ar2.length;
         int ans[] = new int[(ar1.length+ar2.length)];
-       int mem = 0,n1=0,n2=0;
-       for(int i=0;i<ans.length;i++)
-       {
-           if(i<minLength && i%2==0)
-           {
-               ans[i]=ar1[n1];
-               n1++;
-           }
-           if(i<minLength && i%2!=0)
-           {
-               ans[i]=ar2[n2];
-               n2++;
-           }
-
-//           if(i>minLength)
-//           {
-//               if(ar1.length> minLength)
-//               {
-//                   System.out.println(mem);
-//                   ans[i]=ar1[mem-1];
-//
-//               }
-//               else
-//               {
-//                   ans[i]=ar2[mem-1];
-//               }
-//
-//           }
-       }
-        return ans;
+        int n1=0,n2=1,extra=minLength;
+        for(int i=0;i<ans.length;i++) {
+            if(i<minLength) {
+                 ans[n1]=ar1[i];
+                 ans[n2]=ar2[i];
+                 n1+=2;
+                 n2+=2;
+            }
+            else {
+                if (ar1.length > minLength&& extra < ar1.length) {
+                    ans[n1] = ar1[extra];
+                    n1++;
+                } else if (ar2.length >minLength && extra < ar2.length) {
+                    System.out.println(n2);
+                    ans[n2-1] = ar2[extra];
+                    n2++;
+                }
+                extra++;}
+        }return ans;
     }
 }
